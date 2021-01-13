ff1e886c47d98314a60d59585ea4b7d166783719
kacperserewis@outlook.com <kacperserewis@outlook.com>
2021-01-13 15:38:25 +0100

    Prevent shit spam

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect, Background
 from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from stats_repo import StatsRepo
-from mariadb import OperationalError, InterfaceError
+from mariadb import OperationalError, InterfaceError, ProgrammingError
 from ws_connection_manager import WsConnectionManager
 import asyncio, time
 
@@ -32,13 +32,14 @@ try:
 except Exception:
     print("Can't connect to the database!")
 
-    
+
 @app.get("/")
 async def show_server():
     server_addr = getenv("SERVER_HOSTNAME")
     server_port = int(getenv("SERVER_PORT"))
     ms = minestat.MineStat(server_addr, server_port)
     return ms
+
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: str):
@@ -49,13 +50,15 @@ async def websocket_endpoint(websocket: WebSocket, client_id: str):
             await ws_manager.send_personal_message(stats_repo.get_data(), websocket)
     except WebSocketDisconnect:
         ws_manager.disconnect(websocket)
-    
+
+
 @app.exception_handler(OperationalError)
 async def maria_oe_handler(request: Request, exc: OperationalError):
     return JSONResponse(
         status_code=419,
         content={"success": False, "content": exc.__dict__}
     )
+
 
 @app.exception_handler(InterfaceError)
 async def maria_ie_handler(request: Request, exc: InterfaceError):
@@ -71,16 +74,21 @@ async def ws_background():
     last_check = 0
     check_every = 60
     while True:
-        await asyncio.sleep(1)
-        new_data = stats_repo.get_data()
+        try:
+            await asyncio.sleep(1)
+            new_data = stats_repo.get_data()
 
-        if new_data == old_data and int(time.time()) - last_check < check_every:
-            continue
-        print("Broadcasting new information!")
-        old_data = new_data
-        last_check = int(time.time())
-        await ws_manager.broadcast(new_data)
-            
+            if new_data == old_data and int(time.time()) - last_check < check_every:
+                continue
+            print("Broadcasting new information!")
+            old_data = new_data
+            last_check = int(time.time())
+            await ws_manager.broadcast(new_data)
+        except ProgrammingError:
+            print("Can't connect to the database.")
+            await asyncio.sleep(60)
+
+
 @app.on_event('startup')
 async def startup_event():
     stats_repo.get_data()
@@ -88,5 +96,3 @@ async def startup_event():
         ws_background()
     )
     task1
-
-
